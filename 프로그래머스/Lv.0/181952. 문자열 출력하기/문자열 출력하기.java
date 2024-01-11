c80c00e1e08ac27f3f07a83621c298f5c3db4b89
sohee <seobeusohui@gmail.com>
2024-01-11 17:18:47 +0900

    [level Lv.0] Title: 문자열 출력하기, Time: 155.54 ms, Memory: 64.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,17 @@
+//프로그래머스 0단계 문자열 출력하기 
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+        try {
+            //1. 사용자 입력 : Scanner
+            Scanner sc = new Scanner(System.in);
+            //2. 입력 받은 글자 변수 지정 : a
+            String a = sc.next();
+            //3. a 출력
+            System.out.println(a);
+        } catch (Exception e) {
+            //4. 예외 발생 시 메시지 출력
+            System.out.println(e.getMessage());
+        }
+    }
+}
