e5e924a832633a805cf989fa3e71759b1b181cde
Sohee-Elizabeth-park <seobeusohui@gmail.com>
2024-02-16 23:30:53 +0900

    Do it 자료구조와 함께 배우는 알고리즘 입문 <자바 편> 01-1 알고리즘이란? - 연습문제 1-4. 세 값의 대소 관계와 중앙값

---
+++ src/DoitAlgorithm/chap01/Median.java
@@ -0,0 +1,15 @@
+package DoitAlgorithm.chap01;
+//3개 정숫값 입력하고 중앙값 구한 다음 출력
+
+public class Median {
+    //1-1. med3 메서드 설정
+    static int med3(int a, int b, int c) {
+        //1-2. 조건문 if(a >= b)
+        if(a >= b) {
+            if()
+        }
+    }
+    
+    //2-1. 메인 메서드 설정
+    public static void main(String[] args) {}
+}
